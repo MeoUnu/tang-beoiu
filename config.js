4adf898edf219fe70cb115e80c93383e66cb7f61
MeoUnu <86751629+MeoUnu@users.noreply.github.com>
2021-07-01 07:24:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không thích zai khác cơ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: '                           ',
+    reply: 'Suỵt, lí do là em yêu anh chứ gì không cần nhập đâu <3333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Thi cho tốt rồi anh qua đón nhé công chúa đi ăn nhé.',
     btnAccept: 'Okiiiii lun <3',
